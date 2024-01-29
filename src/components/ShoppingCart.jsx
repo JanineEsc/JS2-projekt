b9e72e84955351100d569b034914a699c73aab2b
Janine Escobillo <janineescobillo@hotmail.com>
2024-01-29 17:13:22 +0100

    added a total price and made a link to checkoutpage, added a p tag 'cart empty'

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,29 +1,46 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { CartItem } from "./CartItem"
-
+import { useEffect } from "react";
+import { calcSum, clearCart} from "../store/features/shoppingCart/shoppingCartSlice";
+import { Link } from "react-router-dom";
 
 
 
 
 export const ShoppingCart = () => {
 
-const { cart, totalAmount } = useSelector(state => state.shoppingCart)
+const { cart, totalPrice, } = useSelector(state => state.shoppingCart)
+const dispatch = useDispatch();
+
+useEffect(() => {
+  dispatch(calcSum ());
+}, [dispatch, cart]);
+
+const clearCartFromCart= () => {
+  dispatch(clearCart())
+}
 
   return (
     <div>
+      { cart.length < 1 && (
+        <div className="cartempty">
+          <p> Your Cart is empty</p>
+        </div>
+      )}
       <div className="shoppingCart-container">
         { cart.map(item => (
           <CartItem key={ item.product._id } item={item} />
         ))}
       </div>
-      <hr className="shoppingCart-hr" />
+      <hr className="shoppingCart-hr"></hr> 
       <div className="items-container"> 
       <div>
-        <p>Total price: 0</p>
+        <p>Total price: {totalPrice} SEK</p>
         <small>Inkl. vat</small>
       </div>
-      <div>
-        <button className="checkout-btn"> Checkout </button>
+      <div className="mp-btns">
+        <Link to="/private/checkout" className="checkout-btn"> Checkout </Link>
+        <button onClick={clearCartFromCart} className="clear-btn"> Clear Cart </button>
       </div>
       </div>
 
