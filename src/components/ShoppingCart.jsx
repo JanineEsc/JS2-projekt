f3aa3a56436cbd318c726a28028af3bf06bcea67
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-02-28 22:53:46 +0100

    adds ordernotifications state and conditions for it in the post, useauth token, new div for ordernotification

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { CartItem } from "./CartItem"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { calcSum, clearCart} from "../store/features/shoppingCart/shoppingCartSlice";
 import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/authContext"
@@ -9,9 +9,11 @@ import { useAuth } from "../contexts/authContext"
 
 
 export const ShoppingCart = ( { isCheckoutPage, setIsOpen }) => {
-
+  
+const { token } = useAuth();
 const { cart, totalPrice, } = useSelector(state => state.shoppingCart)
 const dispatch = useDispatch();
+const [orderNotification, setOrderNotification] = useState('')
 
 useEffect(() => {
   dispatch(calcSum ());
@@ -21,15 +23,11 @@ const clearCartOfItems= () => {
   dispatch(clearCart())
 }
 
-const purchaseCompleteAlert = () => {
-  window.alert('Purchase complete');
-};
 
 
-const { token } = useAuth();
 
 const purchaseComplete= async () => {
-  const response = await fetch('https://js2-ecommerce-api.vercel.app/api/orders', {
+  const res = await fetch('https://js2-ecommerce-api.vercel.app/api/orders', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -42,54 +40,60 @@ const purchaseComplete= async () => {
       }))
     })
   });
-
-
     
- // Displays answer from API in the console
-  const responseData = await response.json();
-  console.log(responseData);
-
-
-  clearCartOfItems()
+  const resData = await res.json();
+  if (res.ok) {
+    setOrderNotification('You order has been placed!')
+    clearCartOfItems()
+  } else {
+    setOrderNotification('Something went wrong!')
+  }
+  
+  console.log(resData);
 }
 
 
   return (
-    <div>
-      { cart.length < 1 && (
-        <div className="cartempty">
-          <p> Your Cart is empty</p>
-        </div>
-      )}
-      <div className="shoppingCart-container">
-        { cart.map(item => (
-          <CartItem key={ item.product._id } item={item} />
-        ))}
-      </div>
-      <hr className="shoppingCart-hr"></hr> 
-      <div className="items-container"> 
+    <>
       <div>
-        <p>Total price: {totalPrice} SEK</p>
-        <small>Inkl. vat</small>
+        { cart.length < 1 && (
+          <div className="cartempty">
+            <p> Your Cart is empty</p>
+          </div>
+        )}
+        <div className="shoppingCart-container">
+          { cart.map(item => (
+            <CartItem key={ item.product._id } item={item} />
+          ))}
+        </div>
+        <hr className="shoppingCart-hr"></hr> 
+        <div className="items-container"> 
+          <div>
+            <p>Total price: {totalPrice} SEK</p>
+            <small>Inkl. vat</small>
+          </div>
+          <div className="mp-btns">
+            <button onClick={clearCartOfItems} className="clear-btn"> Clear Cart </button>
+            {
+              isCheckoutPage
+              && 
+              <>
+                <button onClick={() => { purchaseComplete(); clearCartOfItems(); }} className="clear-btn"> Buy</button>
+              </>
+            }
+            { 
+            !isCheckoutPage
+            && 
+            <Link onClick={() => setIsOpen(false)} to="/private/checkout" className="checkout-btn"> Checkout </Link>
+            }
+          </div>
+        </div>
       </div>
-      <div className="mp-btns">
-        <button onClick={clearCartOfItems} className="clear-btn"> Clear Cart </button>
-        {
-          isCheckoutPage
-          && 
-          <>
-            <button onClick={() => { purchaseComplete(); clearCartOfItems(); }} className="clear-btn"> Buy</button>
-          </>
-        }
-        { 
-        !isCheckoutPage
-        && 
-        <Link onClick={() => setIsOpen(false)} to="/private/checkout" className="checkout-btn"> Checkout </Link>
-        }
-        
-      </div>
-      </div>
-
-    </div>
+      {isCheckoutPage &&
+          <div className="order-notification">  
+            <p>{orderNotification}</p>
+          </div>
+          }
+    </>
   )
 }
